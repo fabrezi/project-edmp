6ac8989f858542ec4d86a0bd9f1921cb8043c3ef
fabrezi <fkschon@gmail.com>
2022-10-19 13:46:34 -0400

    db user is hoar

--- app/models/data_agreement/masterDataAgreement.model.js
+++
@@ -1,7 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-  const MasterDataAgreement = sequelize.define("masterDataAgreement", {
-  }, {
-    underscored: true
-  });
-  return MasterDataAgreement;
-};
